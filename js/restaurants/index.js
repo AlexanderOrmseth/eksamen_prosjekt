d80e8955a3661f273330eaccbcfecd6720418601
Alpha Jalloh <alphamartinj@gmail.com>
2021-05-23 14:03:20 +0200

    updated search button

--- js/restaurants/index.js
+++ js/restaurants/index.js
@@ -1,12 +1,20 @@
 // tabs
 const tabs = document.querySelectorAll('.tabs li');
-const searchTxt = document.querySelector("#search-txt");
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
 const tabContent = document.querySelectorAll('#tab-content > div');
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         tabs.forEach(item => item.classList.remove('is-active'))
         tab.classList.add('is-active');
+
+        searchButton.onclick = function() {
+            searchInput.classList.toggle('active')
+            
+        }
+
+       
 
         const target = tab.dataset.target
         tabContent.forEach(content => {
